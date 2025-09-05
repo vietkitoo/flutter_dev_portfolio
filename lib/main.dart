39ff43f8969f594127cd68b414bd9b45ba482b15
vietkitoo <vietkitoo@gmail.com>
2025-09-06 02:12:42 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dev_portfolio/home/home.dart';
 import 'package:flutter_dev_portfolio/routes/app_router.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
-  runApp(
-    ScreenUtilInit(
-      designSize: const Size(390, 844), // kích thước gốc Figma
-      minTextAdapt: true,
-      builder: (context, child) {
-        return const MaterialApp(
-          debugShowCheckedModeBanner: false,
-          home: PinTaskDemo(),
-        );
-      },
-    ),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
